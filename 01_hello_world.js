1486651ff76d04b31d6d65ec6c66a8c40c7792ee
dkfjalsj <1228suhyun@naver.com>
2026-04-30 11:15:32 +0900

    first commit

---
+++ 01_hello_world.js
@@ -0,0 +1,7 @@
+// 한줄 주석
+/**
+ * 여러줄 주석
+ * 이렇게 작성합니다.
+ */
+console.log("안녕하세요")
+console.log("Hello", "world!!")
---
+++ 04_object_type.js
@@ -0,0 +1,10 @@
+const color = {
+  red : '빨강',
+  orange : '주황',
+  yellow : '노랑',
+}
+console.log(typeof color)
+console.log(color)
+console.log('----------------------')
+console.log(color.orange)
+console.log(color['yellow'])
